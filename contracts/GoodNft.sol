e2d88913ceaf8d0378232a926f3b8b3fb060486d
dragonyu1010 <provic0099@gmail.com>
2022-02-23 11:19:46 +0800

    adding setBaseURI in nft contract

--- contracts/GoodNft.sol
+++ contracts/GoodNft.sol
@@ -5,5 +5,19 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract GoodNft is ERC721, Ownable{
+	string public baseURI;
+
 	constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+	function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+	function mint(uint256 tokenId) public {
+		_safeMint(msg.sender, tokenId);
+	}
 }
