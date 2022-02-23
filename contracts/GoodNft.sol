34a8c0266e4c443f70fee75b103af84350ebc11c
dragonyu1010 <provic0099@gmail.com>
2022-02-23 11:01:13 +0800

    add nft contract from ERC721

--- contracts/GoodNft.sol
+++ contracts/GoodNft.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-contract GoodNft {
-	
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract GoodNft is ERC721, Ownable{
+	constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 }
