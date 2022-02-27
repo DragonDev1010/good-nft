9bc0750d3fd05c83b925ebf32a84fb3c676ae62d
dragonyu1010 <provic0099@gmail.com>
2022-02-28 00:09:19 +0800

    add hold token parameter for nft contract migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const GoodNft = artifacts.require("GoodNft");
 
 module.exports = function (deployer) {
-  deployer.deploy(GoodNft, "ARBO Heros", "ARBO");
+  deployer.deploy(GoodNft, "ARBO Heros", "ARBO", "0x41a70A616a35CBFA00Cc0319748F281396366736");
 };
