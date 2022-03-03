8a9b89de5bbc9d68ba072753d57363fd968f5a94
dragonyu1010 <provic0099@gmail.com>
2022-03-03 20:36:25 +0800

    add pause modifier to nft contract

--- contracts/GoodNft.sol
+++ contracts/GoodNft.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721A.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract GoodNft is ERC721, Ownable{
+contract GoodNft is ERC721A, Ownable{
 	IERC721 star;
 	string public baseURI;
 
@@ -24,7 +24,14 @@ contract GoodNft is ERC721, Ownable{
 
 	address public auctionAddress;
 
-	constructor(string memory name, string memory symbol, address starAddr) ERC721(name, symbol) {
+	bool public pause = false;
+
+	modifier notPaused() {
+		require(pause == false, "Mint is paused.");
+		_;
+	}
+
+	constructor(string memory name, string memory symbol, address starAddr) ERC721A(name, symbol) {
 		star = IERC721(starAddr);
 	}
 
@@ -42,46 +49,49 @@ contract GoodNft is ERC721, Ownable{
         baseURI = _newBaseURI;
     }
 
-	function holderMint(uint256[] memory ids) public {
+	function holderMint(uint256[] memory ids) public notPaused {
 		require(totalSupply < 2000, "holderMint : Already all 2000 NFTs were minted.");
 		require(mintStage == 0, "holderMint : Mint stage has to set as Holder Mint Stage.");
 		for(uint256 i = 0 ; i < ids.length ; i++) {
 			require(_checkStarOwner(msg.sender, ids[i]), "holderMint : Only own $STAR can be used to mint.");
 			require(!usedStar[ids[i]],  "holderMint : This $STAR token already used.");
 		}
+
+		_safeMint(msg.sender, ids.length);
+
 		for(uint256 i = 0 ; i < ids.length ; i++) {
-			_safeMint(msg.sender, totalSupply+i);
 			usedStar[ids[i]] = true;
 			starCompMap[ids[i]] = totalSupply+i;
 		}
 		totalSupply += ids.length;
 	}
 
-	function adminMint(address[] memory wallets, uint256[] memory amounts) public onlyOwner {
+	function adminMint(address[] memory wallets, uint256[] memory amounts) public onlyOwner notPaused {
 		require(totalSupply < 2100, "Admin Mint : Admin mint is 2000 ~ 2099 NFTs.");
 		require(mintStage == 1, "Admin Mint : Mint stage has to set as Admin Mint Stage.");
 		require(wallets.length == amounts.length, "Admin Mint : Wallet array has to be match amount array.");
 		for (uint256 i = 0 ; i < wallets.length ; i++) {
 			require(wallets[i] != address(0x0), "Admin Mint : Wallet address can not be zero.");
 			require(amounts[i] > 0, "Admin Mint : Mint amount has to be greater than zero.");
-			for (uint256 j = 0 ; j < amounts[i] ; j++) {
-				_safeMint(wallets[i], totalSupply+j);
-			}
+			
+			_safeMint(wallets[i], amounts[i]);
+			
 			totalSupply += amounts[i];
 		}
 	}
 
-	function influencerMint(uint256 amount, bytes32[] memory proof) public {
+	function influencerMint(uint256 amount, bytes32[] memory proof) public notPaused {
 		require(amount > 0, "Influencer Mint : Mint amount has to be greater than zero.");
 		require(verifyInfluencer(proof), "Influencer Mint : Msg.sender is not registered as Influencer.");
 		require(totalSupply < 2200, "Influencer Mint : Influencer mint is 2100 ~ 2199 NFTs.");
 		require(mintStage == 2, "Influencer Mint : Mint stage has to set as Influencer Mint Stage.");
-		for (uint256 i = 0 ; i < amount ; i++) 
-			_safeMint(msg.sender, totalSupply+i);
+			
+		_safeMint(msg.sender, amount);
+
 		totalSupply += amount;
 	}
 
-	function whitelistMint(uint256 amount, bytes32[] memory proof, uint256 level) public payable {
+	function whitelistMint(uint256 amount, bytes32[] memory proof, uint256 level) public payable notPaused {
 		require(verifyWhitelist(proof, level), "Whitelist Mint : Msg.sender is not registered as Whitelist Level - 1.");
 		require(amount > 0, "Whitelist Mint : Mint amount has to be greater than zero.");
 		require(totalSupply < maxSupply, "Whitelist Mint : Already all 10K NFTs are minted.");
@@ -92,33 +102,31 @@ contract GoodNft is ERC721, Ownable{
 			require(msg.value == presalePrice, "Whitelist Mint : One NFT is 0.042 ETH. Please send correct value for 1 NFT.");
 			// require(verifyWhitelist(proof), "Whitelist Mint : Msg.sender is not registered as Whitelist Level - 1.");
 			
-			_safeMint(msg.sender, totalSupply);
+			_safeMint(msg.sender, 1);
 			totalSupply = totalSupply + 1;
 		} else if (level == 2) {
 			require(amount == 2, "Whitelist Mint : Whitelist Level-2 can mint only 2 NFT.");
 			require(msg.value == 2 * presalePrice, "Whitelist Mint : One NFT is 0.042 ETH. Please send correct value for 2 NFT.");
 			// require(verifyWhitelist(proof), "Whitelist Mint : Msg.sender is not registered as Whitelist Level - 2.");	
 			
-			for (uint256 i = 0 ; i < 2 ; i++) 
-				_safeMint(msg.sender, totalSupply+i);
+			_safeMint(msg.sender, 2);
 			totalSupply += 2;
 		} else {
 			require(amount == 3, "Whitelist Mint : Whitelist Level-3 can mint only 3 NFT.");
 			require(msg.value == 3 * presalePrice, "Whitelist Mint : One NFT is 0.042 ETH. Please send correct value for 3 NFT.");
 			// require(verifyWhitelist(proof), "Whitelist Mint : Msg.sender is not registered as Whitelist Level - 3.");	
 			
-			for (uint256 i = 0 ; i < 3 ; i++) 
-				_safeMint(msg.sender, totalSupply+i);
+			_safeMint(msg.sender, 3);
 			totalSupply += 3;
 		}
 	}
 
-	function publicSale() public onlyOwner{
+	function publicSale() public onlyOwner notPaused {
 		require(totalSupply < maxSupply, "Public Sale : Already all 10K NFTs are minted.");
 		require(mintStage == 4, "Public Sale : Mint stage has to set as public sale Stage.");
 		require(auctionAddress != address(0x0), "Public Sale : Auction contract address is not defined.");
-		for(uint256 i = totalSupply ; i < maxSupply ; i++)
-			_safeMint(auctionAddress, totalSupply+i);
+		
+		_safeMint(auctionAddress, maxSupply - totalSupply);
 	}
 
 	function _checkStarOwner(address minter, uint256 id) private view returns(bool) {
@@ -151,5 +159,9 @@ contract GoodNft is ERC721, Ownable{
 
 	function setAuctionAddress(address auction_) public onlyOwner {
 		auctionAddress = auction_;
+	}
+
+	function setPause(bool pause_) public onlyOwner {
+		pause = pause_;
 	}
 }
