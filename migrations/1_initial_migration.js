94b06bff3ebaff6681b72150b6259a8324d17bcb
dragonyu1010 <provic0099@gmail.com>
2022-03-03 16:22:27 +0800

    add migration for Test721A.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const GoodNft = artifacts.require("GoodNft");
 const MerkleCont = artifacts.require("MerkleCont");
+const Test721A = artifacts.require("Test721A")
 
 module.exports = function (deployer) {
-  deployer.deploy(GoodNft, "ARBO Heros", "ARBO", "0x41a70A616a35CBFA00Cc0319748F281396366736");
-  deployer.deploy(MerkleCont);
+	deployer.deploy(GoodNft, "ARBO Heros", "ARBO", "0x41a70A616a35CBFA00Cc0319748F281396366736");
+	deployer.deploy(MerkleCont);
+	deployer.deploy(Test721A, "ARBO Heros", "ARBO", "0x41a70A616a35CBFA00Cc0319748F281396366736");
 };
